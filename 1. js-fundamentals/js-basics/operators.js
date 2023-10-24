9ced025abf46a2f547c8db9c8a987fc28f2e78e4
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-10-24 21:56:01 +0530

    Operstors 1

--- 1. js-fundamentals/js-basics/operators.js
+++ 1. js-fundamentals/js-basics/operators.js
@@ -61,3 +61,44 @@ console.log(num1 === num2); //false
 
 // Strict Inequality
 console.log(num1 !== num2); //true
+
+// Logical Operators
+// It is used to determine the logic between variables or values.
+
+// Logical Operators
+
+// AND && => ALL OPERANDS ARE TRUE => TRUE
+console.log(true && true && true); // true
+
+// OR || => ATLEAST ONE OPERAND IS TRUE => TRUE
+console.log(true || false); // true
+
+// NOT ! => Reverses the Boolean value
+console.log(!true); // false
+
+// The below line of code outputs 999,
+// because it evaluates from left to right and
+// it returns the last true value in the condition
+console.log("truthy" && 1 && "test" && 999); // 999
+
+// The below line of code outputs 0,
+// because there is one false value in the
+// entire condition and it will always return 0
+console.log("truthy" && 0 && "test" && 999); // 0
+
+// For OR operator
+// The below line returns "truthy" because,
+// when the first value of the condition is true,
+// it doesn't check rest of the values.
+console.log("truthy" || 0 || "test" || 999); // truthy
+
+// The below line returns undefined,
+// because it checks all the values and
+// when all values are false, it returns the last one
+console.log("" || 0 || null || undefined); // undefined
+
+// Assignment Operators
+// Asssignment operators assign a value to its left operand based on the value of right operand.
+let num = 36;
+num += 2;
+console.log(num); // 36
