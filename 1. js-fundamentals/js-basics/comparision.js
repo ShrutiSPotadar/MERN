e39266d4bf3c1636ff301a8d081be3648c132ae4
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-10-25 21:11:22 +0530

    Comparision

---
+++ 1. js-fundamentals/js-basics/comparision.js
@@ -0,0 +1,42 @@
+// Comparision Operator => True / False
+const num1 = 10;
+const num2 = 20;
+
+// Greater than
+console.log(num1 > num2);
+
+// Greater than equal to
+console.log(num1 >= num2);
+
+// Less than
+console.log(num1 < num2);
+
+// Loose equality
+// Doesn't compare the DATA TYPES
+console.log(num1 == num2);
+
+// Not equal
+console.log(num1 != num2);
+
+// Strict Equality
+// Compare VALUES and DATA TYPES
+// Returns true only if both are the same
+console.log(num1 == num2);
+
+// Strict Inequality
+console.log(num1 != num2);
+
+//Logical Operators
+// AND && => ALL OPERANDS ARE TRUE => TRUE
+console.log(true && true && true);
+
+// Atleast one operand is TRUE => TRUE
+console.log(true || false);
+
+// NOT ! => reverse the boolean value
+console.log(!true);
+
+// Assignment Operator
+let num = 25;
+num += 5;
+console.log(num);
