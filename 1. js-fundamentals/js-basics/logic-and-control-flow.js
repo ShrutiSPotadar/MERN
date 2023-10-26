61d0ee52c41af4b6008db3d793c0d4b17585af0b
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-10-26 22:05:37 +0530

    logic and flow control

---
+++ 1. js-fundamentals/js-basics/logic-and-control-flow.js
@@ -0,0 +1,14 @@
+// Conditional Statements
+
+//Conditional statement are used to perform different actions based on different conditions.
+// if,else,if else
+
+const age = 8;
+
+if (age >= 18) {
+  console.log("You may enter");
+} else if (age === 18) {
+  console.log("You have just entered 18!");
+} else {
+  console.log("Grow up");
+}
