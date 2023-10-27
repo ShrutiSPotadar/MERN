7e268b18c2a9d2a9464c50f29b6b78a8052047c8
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-10-27 20:49:34 +0530

    logic and flow control 1

--- 1. js-fundamentals/js-basics/logic-and-control-flow.js
+++ 1. js-fundamentals/js-basics/logic-and-control-flow.js
@@ -12,3 +12,33 @@ if (age >= 18) {
 } else {
   console.log("Grow up");
 }
+
+// FALSY VALUES - Excluding them all other values are TRUTHY VALUES
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
+
+// SOME TRUTHY VALUES
+// true
+// 1
+// "hello"
+// 27 (any number)
+// {} - empty object is also considered to be true
+// [] - empty array is also considered to be true
+
+const points = 10;
+
+if (points) {
+  console.log(`Congrats, you have won thw match by ${points} points`); // Congrats, you have won the match by 10 points.
+} else {
+  console.log("You don't have lost");
+}
+
+// Ternary Operator
+// Syntax
+// (condition) ? true : false
+const years = 18;
+years >= 18 ? console.log("You are eligible to vote") : console.log("You are mot eligible to vote");
