a76a5486f0cacc6eb6139b94eb2a91978e3d41a0
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-10-31 21:43:38 +0530

    functions 1

--- 1. js-fundamentals/js-basics/Fuctions/functions.js
+++ 1. js-fundamentals/js-basics/Fuctions/functions.js
@@ -44,3 +44,32 @@ const mul = (number) => {
 // A short hand to declare arrow function if there is just one statement in the function
 const mul1 = (number) => number * number;
 console.log(mul1(2));
+
+
+// Parameters and Arguments
+// Parameter are used when defining a function.
+// Arguments are passed when making a function call.
+
+const sayHi = (name, age = 23) => {
+  // by default we can make the value of age 23
+  // to avoid undefined as a result when age is not being passed.
+  // name and age are parameters here.
+  console.log(`Hi ${name}, your age is ${age} years old`);
+};
+sayHi("Shruti");
+
+
+// Scope
+// There are three types of scope:
+// 1.Global Scope
+// 2.Function Scope
+// 3.Block Scope
+
+// Global Scope
+// The global scope is the scope that contains, and is visible in, all othe scope.
+
+const name = "Shruti";
+const globalScope = () => {
+  console.log(name);
+};
+globalScope();
