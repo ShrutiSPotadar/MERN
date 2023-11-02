bd3365c01ebdc278b6c38eeebaeb14c3e9bfb81c
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-02 22:08:06 +0530

    functions 3

---
+++ 1. js-fundamentals/js-basics/Fuctions/TypesOfFunctions.js
@@ -0,0 +1,20 @@
+// There are multiple ways of declaring function.
+// A function declaration(defining a function)
+// function name(params)
+// {
+//      statements
+// }
+
+// The advantage of using a function declaration is having a access to this keyword.
+
+// A function expression
+// const name = function(params)
+// {
+//     statements
+// }
+
+// Aarrow function
+// const name = (params) =>
+// {
+//     statements
+// }
