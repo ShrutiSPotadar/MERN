7a8a509f427e99a6be3db794369ba72547d7aecf
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-07 21:56:21 +0530

    string

---
+++ Strings/strings.js
@@ -0,0 +1,7 @@
+// There are three types of strings
+// Strings wrapped up with single and double quotes are just plain txt
+// const single = "Hi, this is a normal text";
+// const double = "Hi, this is also a normal texat";
+
+// String wrapped inside a backtick has extended functionality
+// const backTick = `An extended Functionality provided to strings`;
