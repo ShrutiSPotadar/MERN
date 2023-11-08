f82ee282e7b04192d6a3385c7e66e54c87affa69
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-08 22:29:01 +0530

    string1

--- Strings/strings.js
+++ Strings/strings.js
@@ -1,7 +1,19 @@
 // There are three types of strings
 // Strings wrapped up with single and double quotes are just plain txt
-// const single = "Hi, this is a normal text";
-// const double = "Hi, this is also a normal texat";
+const single = "Hi, this is a normal text";
+console.log(single);
+const doublequote = "Hi, this is also a normal texat";
+console.log(doublequote);
 
 // String wrapped inside a backtick has extended functionality
 // const backTick = `An extended Functionality provided to strings`;
+
+const sum = (a, b) => a + b;
+console.log(`I want to add ${sum(5, 6)}`);
+
+// How to get length of a string
+const firstName = "You are a good person";
+const firstLetter = firstName[0];
+const lastLetter = firstName[firstName.length - 1];
+
+console.log(firstLetter, lastLetter);
