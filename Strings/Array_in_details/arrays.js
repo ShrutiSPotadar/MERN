ea7066a195e4775096f563563bce3a0a0ca767b1
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-15 22:52:51 +0530

    array1

--- Strings/Array_in_details/arrays.js
+++ Strings/Array_in_details/arrays.js
@@ -4,3 +4,20 @@ let months = ["January", "February", "March", "April"];
 for (let i = 0; i < months.length; i++) {
   console.log(months[i]);
 }
+
+// Array Push - Adds a new element containing
+// the enetered value to the end of the array
+months.push("May");
+console.log(months);
+
+// Array Pop - Deletes the last element of an array
+months.pop();
+console.log(months);
+
+// Array Shift - deletes the first element of the array
+months.shift();
+console.log(months);
+
+// Array Unshift - adds the new value to the start of the array
+months.unshift("Samarth");
+console.log(months);
