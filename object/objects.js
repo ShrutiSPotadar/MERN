22305874ed76346244c2791bb012b0fe52db144f
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-27 22:33:48 +0530

    object

---
+++ object/objects.js
@@ -0,0 +1,14 @@
+// Object
+// object is an unordered collection of related date in form of key value pair
+
+const person = {
+  firstName: "Shruti",
+  lastName: "Potadar",
+  age: 23,
+  car: {
+    brand: "Mercedes",
+    year: 2020,
+  },
+};
+
+console.log("person ", person);
