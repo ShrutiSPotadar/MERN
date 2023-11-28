9066c51b31017891e9c516ef3562da129c781038
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-11-28 07:02:03 +0530

    object1

--- object/objects.js
+++ object/objects.js
@@ -12,3 +12,27 @@ const person = {
 };
 
 console.log("person ", person);
+
+// Dot Notation
+console.log("person ", person.car.brand);
+
+// Square Notation
+const property = "age";
+console.log(person[property]);
+
+// Onject.keys
+// object.keys create an aray containing tje keys of an object.
+
+const details = {
+  firstName: "Shruti",
+  lastName: "Potadar",
+  age: 23,
+};
+
+// Let's say we want to see all the positions(keys) in the employee object
+const postions = Object.keys(details);
+console.log(postions);
+
+// Object.values() creates an array containg the values of an object.
+const session = Object.values(details);
+console.log(session);
