00a693377c50b70f03ce2bc223fe12f6f54b46e5
ShrutiSPotadar <shrutipotadar@outlook.com>
2023-12-19 06:17:03 +0530

    DOM

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,11 +1,38 @@
+// DOM(Document Object Model)
+// DOM stands for Document Object Model.
+//  It is a programming interface and representation of structured documents.
+// The DOM represents the structure of a document as a tree of objects,
+// where each object corresponds to a part of the document, such as elements, attributes, text, etc.
+// This model allows programs and scripts to dynamically access and manipulate the content, structure, and style of documents.
+
+// DOM Methods
 // getElementById: This method retrieves an element from the document by its ID.
-
 // querySelector: This method retrieves the first element that matches a specified CSS selector.
-
 // querySelectorAll: This method retrieves all elements that match a specified CSS selector.
-
 // getElementsByTagName: This method retrieves all elements with a specified tag name.
-
 // getElementsByClassName: This method retrieves all elements with a specified class name.
+// Here are some examples of how you might use these methods:
 
 // Here are some examples of how you might use these methods:
+const element = document.getElementById("my-element");
+const firstPara = documet.querySelector("p");
+const item = document.querySelector("li");
+const divs = document.getElementsByTagName("div");
+const classElement = document.getElementsByClassName("red");
+
+// Here is a list of some common DOM (Document Object Model)
+// properties and methods that you can use to manipulate HTML elements:
+
+// Properties
+// innerHTML: This property gets or sets the HTML content within an element.
+// style: This property gets or sets the inline style of an element.
+// className: This property gets or sets the class name of an element.
+// id: This property gets or sets the ID of an element.
+// value: This property gets or sets the value of an input element.
+
+// Methods
+// getAttribute: This method gets the value of an attribute of an element.
+// setAttribute: This method sets the value of an attribute of an element.
+// addEventListener: This method adds an event listener to an element.
+// removeEventListener: This method removes an event listener from an element.
+// appendChild: This method adds a new child element to an element.
